a8bba9ba43df0bc2836587f3a5dea0d9e325e382
Ivan <ereyzer142@gmail.com>
2025-07-02 14:51:26 +0300

    footer component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 
 
 import './App.css'
+import Footer from './footer/Footer'
 import Header from './header/Header'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   )
 }
