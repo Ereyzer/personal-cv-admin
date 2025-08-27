6c71cc857f43cd0d40fc4b7cbc260605f9186dcb
Ivan <ereyzer142@gmail.com>
2025-08-27 22:18:09 +0300

    fix navigate err

--- src/pages/StatPage.jsx
+++ src/pages/StatPage.jsx
@@ -1,3 +1,4 @@
+import { Navigate } from 'react-router-dom';
 import { useUser } from '../components/context/user/userContext';
 import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
